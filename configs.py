65de4f315284b19853d46977534f48f4de0a8b83
LieStage <118051769+LieStage@users.noreply.github.com>
2023-09-30 20:40:47 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,10 +13,10 @@ class Config(object):
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "945284066"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://misoc51233:filestore@cluster0.imfn5pn.mongodb.net/?retryWrites=true&w=majority")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "movie_time_botonly")
-  LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001634620304")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001634620304"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-  BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+  BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
   ABOUT_BOT_TEXT = f"""
